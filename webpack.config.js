951bfe10dff962359cbcb7854c119ecf8a4b4741
zoe <lizhou1021@qq.com>
2017-11-29 17:33:39 +0800

    修复bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const webpack = require('webpack');
 var path = require('path');
+
+var HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: __dirname+"/src/index.tsx",
   output: {
       filename: "bundle.js",
       path: __dirname + "/dist"
   },
-  watch: true,
   // Enable sourcemaps for debugging webpack's output.
   devtool: "eval-source-map",
 
@@ -22,8 +24,6 @@ module.exports = {
       rules: [
           // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
           { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-
           // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
           { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
           {
@@ -38,23 +38,14 @@ module.exports = {
           }
       ]
   },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
-  },
   devServer: {
-    contentBase: "./",//本地服务器所加载的页面所在的目录
-    historyApiFallback: true,//不跳转
-    inline: true,//实时刷新
-    port:"9527",
-    hot: true
+    contentBase: "./dist/",//本地服务器所加载的页面所在的目录
+    // historyApiFallback: true,//不跳转
+    // inline: true,//实时刷新
+    port:"9527"
   } ,
   plugins: [
-    new webpack.HotModuleReplacementPlugin()//热加载插件
+    // new webpack.HotModuleReplacementPlugin()//热加载插件
+    new HtmlWebPackPlugin({ title: "webCAD" ,template:"./index.html"}),
   ],
 };
