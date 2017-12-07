154539686ff189ed1ef58d4a6c539c6a85deb623
Zoe <lizhou1021@qq.com>
2017-12-07 22:50:03 +0800

    learn three

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,38 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
 import './style.css';
-import { Hello } from "./components/Hello";
-import * as THREE from "Three";
 
+import * as React from 'react';
+import * as THREE from 'Three';
+
+// 添加场景
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// 添加相机
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 0, 5);
+// const camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 1, 10);
+// camera.position.set(4, -3, 5);
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+scene.add(camera);
+
+//添加渲染器
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0x000);
 
-camera.position.z = 5;
-var animate = function () {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    renderer.render(scene, camera);
-};
-
-animate();
-
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+//添加长方体
+var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true
+    })
 );
+scene.add(cube);
+
+renderer.render(scene, camera);
+
+// ReactDOM.render(
+//     ,
+//     document.getElementById("example")
+// );
