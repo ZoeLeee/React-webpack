e5ff97ae84cab7a4559bd87b8e84853ae0cff257
Zoe <lizhou1021@qq.com>
2017-12-10 23:07:18 +0800

    学习line

--- src/component/line.ts
+++ src/component/line.ts
@@ -1,17 +1,119 @@
 import * as THREE from "Three";
-
-export function line(scene: THREE.Scene)
+export function createLine()
 {
+    let mouseX = 0, mouseY = 0;
+    let windowHalfX = window.innerWidth / 2;
+    let windowHalfY = window.innerHeight / 2;
 
-    var material = new THREE.LineBasicMaterial({
-        color: 0x0000ff
-    })
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(
-        new THREE.Vector3(-10, 10, 0),
-        new THREE.Vector3(0, -10, 0),
-        new THREE.Vector3(10, 0, 0),
-    )
-    var line = new THREE.Line(geometry, material);
-    scene.add(line);
+    const SEPARATION = 200;
+    const AMOUNTX = 10;
+    const AMOUNTY = 10;
+    let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.CanvasRenderer;
+    function init()
+    {
+        //创建节点
+        let separation = 100;
+        let amountX = 50;
+        let amountY = 50;
+        let particles, particle;
+        let container = document.createElement('div');
+        //创建相机
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+        camera.position.z = 100;
+        //创建场景
+        scene = new THREE.Scene();
+
+        //创建渲染器
+        renderer = new THREE.CanvasRenderer();
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        container.appendChild(renderer.domElement);
+        renderer.render(scene, camera);
+
+        //particles 粒子
+
+        const PI2 = Math.PI * 2;
+        //材质
+        let material = new THREE.SpriteCanvasMaterial({
+            color: 0xffffff,
+            program: function (context)
+            {
+                context.beginPath();
+                context.arc(0, 0, 0.5, 0, PI2, true);//描圆
+                context.fill();
+            }
+        })
+        //创建几何体
+        let geometry = new THREE.Geometry();
+        for (let i = 0; i < 100; i++)
+        {
+            particle = new THREE.Sprite(material);
+            particle.position.x = Math.random() * 2 - 1;
+            particle.position.y = Math.random() * 2 - 1;
+            particle.position.z = Math.random() * 2 - 1;
+            particle.position.normalize();
+            particle.position.multiplyScalar(Math.random() * 10 + 450);
+            particle.scale.x = particle.scale.y = 10;
+            scene.add(particle);
+            geometry.vertices.push(particle.position);
+        }
+
+        //line
+        let line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
+            color: 0xffffff,
+            opacity: 0.5
+        }));
+        scene.add(line);
+        document.addEventListener('mousemove', onDocumentMouseMove, false);
+        document.addEventListener('touchstart', onDocumentTouchStart, false);
+        document.addEventListener('touchmove', onDocumentTouchMove, false);
+        window.addEventListener('resize', onWindowResize, false);
+    }
+    function onWindowResize()
+    {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    function onDocumentMouseMove(event: MouseEvent)
+    {
+        mouseX = event.clientX - windowHalfX;
+        mouseY = event.clientY - windowHalfY;
+    }
+    function onDocumentTouchStart(event: TouchEvent)
+    {
+        if (event.touches.length > 1)
+        {
+            event.preventDefault();
+            mouseX = event.touches[0].pageX - windowHalfX;
+            mouseY = event.touches[0].pageY - windowHalfY;
+        }
+    }
+    function onDocumentTouchMove(event: TouchEvent)
+    {
+        console.log(event)
+        if (event.touches.length == 1)
+        {
+            event.preventDefault();
+            mouseX = event.touches[0].pageX - windowHalfX;
+            mouseY = event.touches[0].pageY - windowHalfY;
+        }
+    }
+    function animate()
+    {
+        requestAnimationFrame(animate);
+        render();
+    }
+    function render()
+    {
+        camera.position.x += (mouseX - camera.position.x) * .05;
+        camera.position.y += (- mouseY + 200 - camera.position.y) * .05;
+        camera.lookAt(scene.position);
+        renderer.render(scene, camera);
+    }
+
+    init();
+    animate();
 }
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
 
-import { line } from './component/line';
+import { createLine } from './component/line';
 import * as THREE from 'Three';
+createLine();
+// // 添加场景
+// const scene = new THREE.Scene();
 
-// 添加场景
-const scene = new THREE.Scene();
+// // 添加相机
+// const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+// // camera.position.set(1, 0, 10);
+// // const camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 1, 10);
+// camera.position.set(0, 0, 50);
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
+// scene.add(camera);
 
-// 添加相机
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-// camera.position.set(1, 0, 10);
-// const camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 1, 10);
-camera.position.set(0, 0, 50);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-scene.add(camera);
+// line(scene);
 
-line(scene);
-
-//添加渲染器
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-renderer.setClearColor(0x000);
-renderer.render(scene, camera);
+// //添加渲染器
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+// renderer.setClearColor(0x000);
+// renderer.render(scene, camera);
 
 
 
